f6af67f990bff1fd7095a9ccf9c98f45bfe645ab
Sagar Kumar (IN64224) <m1064224@mindtree.com>
2021-11-24 15:36:52 +0530

    commit

--- src/Modernization.java
+++ src/Modernization.java
@@ -1,9 +1,16 @@
+import com.adidas.Employee;
 
 public class Modernization {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sagar Kumar");
+		
+		Employee emp = new Employee();
+		emp.setEid(0);
+		emp.setEname("Name");
+		
+		System.out.println(emp.getEid()+" "+emp.getEname()+" "+emp.getClass().getName());
 	}
 
 }
