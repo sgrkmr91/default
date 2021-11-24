7ea905dfa83b467d3d9c0d3eef7088cf134ee2b6
Sagar Kumar (IN64224) <m1064224@mindtree.com>
2021-11-24 14:58:18 +0530

    For checkout

--- src/com/adidas/Employee.java
+++ src/com/adidas/Employee.java
@@ -3,6 +3,7 @@ package com.adidas;
 public class Employee {
 
 	private int eid;
+	private String ename;
 	public int getEid() {
 		return eid;
 	}
@@ -15,6 +16,33 @@ public class Employee {
 	public void setEname(String ename) {
 		this.ename = ename;
 	}
-	private String ename;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + eid;
+		result = prime * result + ((ename == null) ? 0 : ename.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (eid != other.eid)
+			return false;
+		if (ename == null) {
+			if (other.ename != null)
+				return false;
+		} else if (!ename.equals(other.ename))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
